4745cbe8375509ac8ef3713c83a17fd5c11c5e7d
vuluu <vuluu040320@gmail.com>
2023-11-09 14:50:55 +0700

    fix docker

--- server/src/configs/database/index.ts
+++ server/src/configs/database/index.ts
@@ -1,13 +1,13 @@
 // getting-started.js
 import mongoose from 'mongoose';
 
-const dbHost = process.env.DB_HOST || 'localhost'
-const dbPort = process.env.DB_PORT || 27017
-const dbName = process.env.DB_NAME || 'auth_db'
-const dbUser = process.env.DB_USER
-const dbUserPassword = process.env.DB_PASSWORD
+// const dbHost = process.env.DB_HOST || 'localhost';
+const dbPort = process.env.DB_PORT || 27017;
+const dbName = process.env.DB_NAME || 'auth_db';
+// const dbUser = process.env.DB_USER;
+// const dbUserPassword = process.env.DB_PASSWORD;
 
-const mongoUrl = `mongodb://${dbUser}:${dbUserPassword}@${dbHost}:${dbPort}/${dbName}`
+const mongoUrl = `mongodb://auth-mongo:${dbPort}/${dbName}`;
 
 console.log(mongoUrl);
 
