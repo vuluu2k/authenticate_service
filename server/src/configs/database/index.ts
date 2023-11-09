9fd41f4e76b12686aeee99dee1c6dd9ec10bfdca
vuluu <vuluu040320@gmail.com>
2023-11-09 08:33:50 +0700

    add docker

--- server/src/configs/database/index.ts
+++ server/src/configs/database/index.ts
@@ -1,11 +1,19 @@
 // getting-started.js
 import mongoose from 'mongoose';
 
+const dbHost = process.env.DB_HOST || 'localhost'
+const dbPort = process.env.DB_PORT || 27017
+const dbName = process.env.DB_NAME || 'auth_db'
+const dbUser = process.env.DB_USER
+const dbUserPassword = process.env.DB_PASSWORD
+
+const mongoUrl = `mongodb://${dbUser}:${dbUserPassword}@${dbHost}:${dbPort}/${dbName}`
+
+console.log(mongoUrl);
+
 async function connectDB() {
   try {
-    await mongoose.connect(
-      'mongodb+srv://dev040320:SpCThGd9YsCJX7TB@authenticated.15bhymr.mongodb.net/?retryWrites=true&w=majority'
-    );
+    await mongoose.connect(mongoUrl);
 
     console.log('connect db success');
   } catch (error) {
